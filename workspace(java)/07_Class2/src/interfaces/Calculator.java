30f8a26fb2d7c8b219420eff6c4b880d7dd3fc86
hyebin-seo <rhfqlsdkdl18@gmail.com>
2021-03-11 18:40:13 +0900

    210311-home-2

--- workspace(java)/07_Class2/src/interfaces/Calculator.java
+++ workspace(java)/07_Class2/src/interfaces/Calculator.java
@@ -1,5 +1,7 @@
 package interfaces;
 
 public class Calculator {
+	
+	int add(int su1, int su2);
 
 }
--- workspace(java)/07_Class2/src/interfaces/Ex01_inter.java
+++ workspace(java)/07_Class2/src/interfaces/Ex01_inter.java
@@ -10,6 +10,17 @@ public class Ex01_inter {
 		Sub sub = new Sub();
 		
 		// static final 상수로 인식
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		// 인스턴스의 멤버변수는 상수로 선언되기 때문에 값을 변경할 수 없다.
 		// sub.num = 250;
 
